df27c94d2fd0d5c39c9662a8db21635bc06495a7
K R omshankar <you@example.com>
2022-11-15 13:18:50 +0530

    contextAPI

--- src/ContextAPI-EX/Display.jsx
+++ src/ContextAPI-EX/Display.jsx
@@ -4,16 +4,20 @@ import { store } from './App1'
 
 const Display = () => {
   const [data,Setdata]=useContext(store)
+  const [name,Setname]=useState("")
+  const submithandler=((e)=>{
+    Setname("")
+    e.target.reset()
+e.preventDefault()
+Setdata([...data,{Name:name}])
+  })
   return (
     <div>
-      Display
-      {
-        data.map((name)=>{
-          return(<p>{name.Name}</p>
-
-          )
-        })
-      }
+    { data.map((name)=>{return(<p>{name.Name}</p>)})}
+      <form onSubmit={submithandler}>
+      <input type="text" placeholder='Enter Name' onChange={(e)=>{Setname(e.target.value)}} />
+      <input type="submit"  value="Add"/>
+      </form>
     </div>
   )
 }
